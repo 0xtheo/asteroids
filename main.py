70372e5b81c358f727e3ff814c53cc72d20e9bec
0xtheo <theo.sarran@gmail.com>
2025-06-20 18:58:50 -0700

    ship rotates

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000
+        player.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
